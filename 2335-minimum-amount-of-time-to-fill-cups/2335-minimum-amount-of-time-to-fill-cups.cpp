d6fa4a4dc77cf4a9ff2144c9414635a8fbc43e91
Aaryan Singh <www.aaryan000singh@gmail.com>
2026-07-25 18:42:15 +0530

    Time: 0 ms (100%), Space: 14 MB (81.55%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -3,21 +3,22 @@ public:
     int fillCups(vector<int>& amount) {
         priority_queue<int> pq;
         for(int i = 0; i < 3; i++){
-            if(amount[i] != 0) pq.push(amount[i]);
+            if(amount[i] != 0) pq.push(amount[i]); //only store non zero values in the pq
         }
         int time = 0;
         while(!pq.empty()){
             int fcup = pq.top();
-            pq.pop();
+            pq.pop(); //after popping fcup if the q become empty then it means only fcup is in the queue
             if(pq.empty()){
-                time += fcup;
-                break;
+                time += fcup; //add the time cuz we can fill 1 cup per sec
+                break; //break from the loop
             }
-            int scup = pq.top();
+            //if queue is not empty.
+            int scup = pq.top(); //second cup
             pq.pop();
-            if(fcup > 1) pq.push(fcup-1);
+            if(fcup > 1) pq.push(fcup-1); //if fcup freq is more than 1 then only push it back in the queue with one decrement as we've filled it.
             if(scup > 1)pq.push(scup-1);
-            time++;
+            time++; //1 sec passed if we filled fcup and scup
         }
         return time;
     }
