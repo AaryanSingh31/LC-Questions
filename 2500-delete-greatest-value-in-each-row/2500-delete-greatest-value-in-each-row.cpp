0d8c8780d3040f4998bb6c27b8aad528b090721d
Aaryan Singh <www.aaryan000singh@gmail.com>
2026-07-27 19:10:53 +0530

    Time: 4 ms (22.82%), Space: 12.9 MB (77.33%) - LeetHub

--- 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
@@ -5,17 +5,18 @@ public:
         int n = grid[0].size(); //col size
 
         for(int i = 0; i < m; i++){
-            sort(grid[i].begin(), grid[i].end());
+            sort(grid[i].begin(), grid[i].end()); //row wise sorting
         }
         int ans = 0;
 
+//loop for col to row and not from row to col
         for(int j = 0; j < n; j++){
             int maxEl = 0;
 
             for(int i = 0; i < m; i++){
-                maxEl = max(maxEl, grid[i][j]);
+                maxEl = max(maxEl, grid[i][j]); //max el of each col
             }
-            ans += maxEl;
+            ans += maxEl; //add it to the ans after evry end of the row visit cuz that is the 1 col visit
         }
         return ans;
     }
