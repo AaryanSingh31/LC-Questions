b9b61dff2b2593b898b4aca68223dd07ac99bc0a
Aaryan Singh <www.aaryan000singh@gmail.com>
2026-07-28 23:41:11 +0530

    Time: 43 ms (83.36%), Space: 157.1 MB (80.16%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -7,13 +7,14 @@ public:
 
             vector<vector<int>> ans;
         for(int i = 0; i < min((int)nums1.size(), k); i++){
-            pq.push({nums1[i] + nums2[0], i , 0});
+            pq.push({nums1[i] + nums2[0], i , 0}); //Visualise a matrix of added numbers .. push all the first numbers of each row or till k
         }
         while(k > 0 && !pq.empty()){
-            auto [sum, i , j] = pq.top();
+            auto [sum, i , j] = pq.top(); //i is the index of nums1, j is of nums2
             pq.pop();
 
-            ans.push_back({nums1[i], nums2[j]});
+            ans.push_back({nums1[i], nums2[j]}); //push back the nums to the ans
+            //push the next element from the same row of the matrx from which the top is popped
             if(j+1 < nums2.size()){
                 pq.push({nums1[i]+nums2[j+1], i , j+1});
             }
