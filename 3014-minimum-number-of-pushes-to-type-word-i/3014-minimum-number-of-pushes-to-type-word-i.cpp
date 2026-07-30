1780678eb97318672b17277c066c002e7250c264
Aaryan Singh <www.aaryan000singh@gmail.com>
2026-07-31 00:28:35 +0530

    Time: 0 ms (100%), Space: 8.3 MB (54.67%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+        if(n <= 8){
+            return n;
+        }else if( n > 8 && n <= 16){
+            return 8 + 2 * (n - 8);
+        }else if(n > 16 && n <= 24){
+            return 8 + (2 * 8) + 3 * (n - 16);
+        }else {
+            return 8 + 16 + 24 + (n - 24) * 4;
+        }
+        return -1;
+    }
+};
