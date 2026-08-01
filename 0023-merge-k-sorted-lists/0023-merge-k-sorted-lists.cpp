cfbf542541271383d694ee41a0d9b08c627e2795
Aaryan Singh <www.aaryan000singh@gmail.com>
2026-08-01 19:30:23 +0530

    Time: 3 ms (61.41%), Space: 18.4 MB (82.7%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -22,17 +22,17 @@ public:
             pq.push(lists[i]);
         }
         ListNode* dummy = new ListNode(-1);
-        ListNode * temp = dummy;
+        ListNode * temp = dummy; //since temp = dummy so dummy's next will be the first node.
         
         while(!pq.empty()){
-            ListNode* node = pq.top();
+            ListNode* node = pq.top(); //top smallestt node
             pq.pop();
-            if(node->next){
+            if(node->next){ //if its next list exists push it
             pq.push(node->next);
             }
-            temp->next = node;
-            temp = temp->next;
+            temp->next = node; //add the node into the final list using temp ptr
+            temp = temp->next; //move the temp ptr
         }
-        return dummy->next;
+        return dummy->next; //ie, head of the temp list is dummy->next
     }
 };
