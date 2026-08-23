120afcdf92e9f8ab2beef74d950c1b14849d5ec9
Aaryan Singh <www.aaryan000singh@gmail.com>
2026-08-23 11:39:39 +0530

    Time: 20 ms (19.52%), Space: 94.7 MB (11.22%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -6,14 +6,8 @@ public:
         if(org.size() != m * n){
             return empty;
         }
-        int r1 = 0,  c1 = 0;
         for(int i = 0; i < org.size(); i++){
-            ans[r1][c1] = org[i];
-            c1++;
-            if(c1 == n){
-                r1++;
-                c1 = 0;
-            }
+            ans[i/n][i%n] = org[i];
         }
         return ans;
     }
